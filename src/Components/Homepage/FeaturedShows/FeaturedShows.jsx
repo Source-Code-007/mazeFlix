d78cf8fab8fd317299d9c203ac938ea8d3eed15f
Source-Code-007 <utsho926@gmail.com>
2023-06-02 00:39:28 +0600

    featured show section implement

---
+++ src/Components/Homepage/FeaturedShows/FeaturedShows.jsx
@@ -0,0 +1,59 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+import { useEffect, useState } from "react";
+
+const FeaturedShows = () => {
+    const [loading, setLoading] = useState(true)
+    const [shows, setShows] = useState([])
+    useEffect(() => {
+        fetch('https://api.tvmaze.com/search/shows?q=all')
+            .then(res => res.json())
+            .then(data => {
+                setShows(data)
+                setLoading(false)
+                console.log(data);
+            })
+    }, [])
+
+
+    return (
+        <div className="py-20">
+            <div className="w-5/6 md:3/6 xl:w-2/6 mx-auto py-14 text-center">
+                <h2 className="font-bold text-xl text-red-500">Featured</h2>
+                <p className="font-semibold text-6xl text-slate-300"><span className="text-white font-bold text-7xl">Now</span> Showing</p>
+            </div>
+            {
+                loading ? ' ' : <Swiper
+                    slidesPerView={4}
+                    spaceBetween={30}
+                    centeredSlides={true}
+                    pagination={{
+                        clickable: true,
+                    }}
+                    modules={[Pagination]}
+                    className="mySwiper"
+                >
+                    {shows.map((show, ind) => {
+                        const {name, image} = show.show
+                        return <SwiperSlide key={ind} className="mb-8">
+                            <div className="card card-compact bg-transparent shadow-inner shadow-red-500">
+                                <figure><img className="h-[400px] w-full " src={image.original} /></figure>
+                                <div className="card-body">
+                                    <h2 className="card-title">{name}</h2>
+                                    <div className="card-actions justify-end">
+                                        <button className="my-btn">Details</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    })}
+                </Swiper>
+            }
+
+        </div>
+    );
+};
+
+export default FeaturedShows;
--- src/Components/Homepage/Homepage.jsx
+++ src/Components/Homepage/Homepage.jsx
@@ -1,9 +1,11 @@
 import Banner from "./Banner/Banner";
+import FeaturedShows from "./FeaturedShows/FeaturedShows";
 
 const Homepage = () => {
     return (
         <>
             <Banner></Banner>
+            <FeaturedShows></FeaturedShows>
         </>
     );
 };
